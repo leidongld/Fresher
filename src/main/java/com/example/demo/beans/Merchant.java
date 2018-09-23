8324adbe885fae0a1933b8491f3a88d8c54de382
leidong <leidongld@sina.com>
2018-09-23 22:11:44 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/beans/Merchant.java
@@ -0,0 +1,10 @@
+package com.example.demo.beans;
+
+/**
+ * @program: Fresher
+ * @description: 商户
+ * @author: Lei Dong
+ * @create: 2018-09-23 22:10
+ **/
+public class Merchant {
+}
---
+++ src/main/java/com/example/demo/beans/Product.java
@@ -0,0 +1,99 @@
+package com.example.demo.beans;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * @program: Fresher
+ * @description: 商品
+ * @author: Lei Dong
+ * @create: 2018-09-23 22:04
+ **/
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue
+    private String id;
+
+    private String categoryId;
+
+    private String name;
+
+    private String imagePath;
+
+    private float price;
+
+    private String detail;
+
+    public Product(String id, String categoryId, String name, String imagePath, float price, String detail) {
+        this.id = id;
+        this.categoryId = categoryId;
+        this.name = name;
+        this.imagePath = imagePath;
+        this.price = price;
+        this.detail = detail;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", categoryId='" + categoryId + '\'' +
+                ", name='" + name + '\'' +
+                ", imagePath='" + imagePath + '\'' +
+                ", price=" + price +
+                ", detail='" + detail + '\'' +
+                '}';
+    }
+}
